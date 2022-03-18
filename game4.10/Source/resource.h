6cf75da7d5febdad2266cd2b1a17f066b8a1752c
983529297 <sam22822964@gmail.com>
2022-03-18 11:17:23 +0800

    upgrade

--- game4.10/Source/resource.h
+++ game4.10/Source/resource.h
@@ -43,6 +43,8 @@
 #define IDB_TOWER1                      165
 #define IDB_BITMAP5                     166
 #define IDB_TOWER2                      166
+#define IDB_BITMAP6                     167
+#define IDB_E_DOG_ACT1                  167
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -55,7 +57,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        167
+#define _APS_NEXT_RESOURCE_VALUE        168
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
